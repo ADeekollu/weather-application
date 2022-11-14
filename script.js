b63a4e35f64163c0e6d4255c6258c9a07ef9a2f6
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-11-14 17:11:19 -0500

    added containers to html plus styling, added fetch call and local storage

--- script.js
+++ script.js
@@ -1,27 +1,108 @@
 
 
 var searchButton = document.getElementById("button")
-var weatherApiKey = "601c8b09d7aad2ea141da9a258a4c34d"
+var apiKey = "601c8b09d7aad2ea141da9a258a4c34d"
+var cityFormEl = $("#cityInputForm")
+var cityListEl = $("#cityList")
+var input = $("#cityInput")
+var cityInputEl = ""
+var currentDate = moment().format("MMM Do, YYYY")
+var searchedCities = (localStorage.getItem("city"))
+    ? JSON.parse(localStorage.getItem("city"))
+    : [];
+
+function renderCityList() {
+    cityListEl.html("")
+    $(searchedCities).each(function(i,el){
+        cityListEl.prepend("<li>" + el + "</li>")
+    })
+    $("li").attr("class", "list-group-item list-group-item-action")
+    $("li").on("click", function(){
+        var liEl = $(this).text()
+        cityInputEl = liEl
+        getWeather()
+    })
+}
+renderCityList();
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    cityInputEl = $("#cityInput").val().trim()
+    if (!searchedCities.includes(cityInputEl)) {
+        searchedCities.push(cityInputEl)
+    }
+    if (!cityInputEl){
+        return
+    }
+    renderCityList();
+    $("#cityName").text(cityInputEl)
+    $("input[name = 'cityNameInput']").val("")
+    localStorage.setItem("city", JSON.stringify(searchedCities))
+}
+cityFormEl.on("submit", handleFormSubmit)
 
 
+function getWeather() {   
+    var requestUrlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&units=imperial&appid=" + apiKey;
 
 
-function getWeather() {
-    var newName = document.getElementById("cityInput")
-    var cityName = document.getElementById("cityName")
-    cityName.innerHTML = newName.value
-
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'appid=601c8b09d7aad2ea141da9a258a4c34d")
+    fetch(requestUrlCurrent)
     .then(function (response){
         return response.json();
     })
     .then(function(data) {
         console.log(data);
+        var cityName = data.name
+        var latEl = data.coord.lat
+        var lonEl = data.coord.lon
+        var requestUrl5Day = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityInputEl + "&appid=" + apiKey
+        var requestUrlOneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEl + "&lon=" + lonEl + "&exclude=minutely,hourly,alerts&units=imperial&appid=" + apiKey
+
+        $("#cityName").text(cityName)
+        $("#currentDate").text(moment().add(1, 'day').format("MMM Do, YYYY"));
+        $("#weatherIcon").attr({src:"http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+        $("#currentTemp").text("Temp: " + data.main.temp + "°");  
+        $("#currentHum").text("Humidity: " + data.main.humidity + "%");
+        $("#currentWind").text("Wind: " + data.wind.speed + "mph");
+        console.log()
+
+
+        fetch(requestUrl5Day)
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            console.log(data);
+            
+            // set dates for 5-day forecast
+            $("#forecastDate1").text(moment().add(1, 'day').format("MMM Do, YYYY"));
+            $("#forecastDate2").text(moment().add(2, 'day').format("MMM Do, YYYY"));
+            $("#forecastDate3").text(moment().add(3, 'day').format("MMM Do, YYYY"));
+            $("#forecastDate4").text(moment().add(4, 'day').format("MMM Do, YYYY"));
+            $("#forecastDate5").text(moment().add(5, 'day').format("MMM Do, YYYY"));                       
+            
+            $("#forecastIcon1").attr({src:"http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+            $("#forecastIcon2").attr({src:"http://openweathermap.org/img/wn/" + data.list[1].weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+            $("#forecastIcon3").attr({src:"http://openweathermap.org/img/wn/" + data.list[2].weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+            $("#forecastIcon4").attr({src:"http://openweathermap.org/img/wn/" + data.list[3].weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+            $("#forecastIcon5").attr({src:"http://openweathermap.org/img/wn/" + data.list[4].weather[0].icon + "@2x.png", "width":"50px", "height":"50px"});
+
+            
+
+        })
     })
 }
 
+        // for (var i = 0; i < 5; i++) {
+        // document.getElementById("day_" + (i+1) + "_temp").innerHTML = "Temp :" + Number(data.list[i].main.temp - 288.53).toFixed(1) + "°"
+        // document.getElementById("day_" + (i+1) + "_humidity").innerHTML = "Humidity :" + Number(data.list[i].main.humidity) + "%"
 
-searchButton.addEventListener("click", getWeather)
+
+
+
+
+
+cityFormEl.on("submit", getWeather)
 
 
 
